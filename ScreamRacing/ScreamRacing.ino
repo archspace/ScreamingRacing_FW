d8bdb33c4d94a9a9044d9559cfc1d6259e3abc87
alinyo <alinyo.mail@gmail.com>
2017-10-24 21:25:24 +0800

    Receive a buffer with length 8 via BLE

--- ScreamRacing/ScreamRacing.ino
+++ ScreamRacing/ScreamRacing.ino
@@ -29,6 +29,15 @@
 
 SoftwareSerial BTSerial(2, 3); // RX, TX
 
+static void LOG(const char *fmt, ... ){
+    char tmp[128]; // resulting string limited to 128 chars
+    va_list args;
+    va_start (args, fmt );
+    vsnprintf(tmp, 128, fmt, args);
+    va_end (args);
+    Serial.print(tmp);
+}
+
 void setup() {
     // Open serial communications and wait for port to open:
     Serial.begin(9600); //Sugguest to set the same baud rate with software  serial part
@@ -39,16 +48,36 @@ void setup() {
   
     // set the data rate for the SoftwareSerial port
     BTSerial.begin(9600);
-  
+
+#if TEST
     Serial.println("AT+RX");
     BTSerial.write("AT+RX");
+    while(BTSerial.available()) {
+        LOG("%c", (char)BTSerial.read());
+    }
+#endif
+    Serial.println("BLE init done");
 }
 
+uint8_t buf[10];
+int ret;
 void loop() { // run over and over
     //Receive data from BT Serial UART
     if (BTSerial.available()) {
-        //BTSerial.read();
-        Serial.println((char)BTSerial.read());
+#if TEST
+        LOG("%c", (char)BTSerial.read());
+#else
+        ret = BTSerial.readBytes(buf, 8);
+        if (ret == 8) {
+            LOG("F_LED: %d, F_LED: %d, M_LED: %d, M_LED: %d\n"
+                  "L motor: %d, speed: %d\n"
+                  "R motor: %d, speed: %d\n",
+                  buf[0], buf[1], buf[2], buf[3],
+                  buf[4], buf[5], buf[6], buf[7]);
+        } else {
+            LOG("Wrong command!\n");
+        }
+#endif
     }
     // Receive data from command line for debug --> BT 
     if (Serial.available()) {
